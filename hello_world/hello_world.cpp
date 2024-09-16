b8a3492ef20917ba60806d61093920d1d143f908
huang214 <huang214@msu.edu>
2024-09-16 10:42:12 -0400

    hello world

---
+++ hello_world/hello_world.cpp
@@ -0,0 +1,9 @@
+// Tutorial code to try to compile and run
+
+#include <iostream>
+
+int main(){
+    std::cout << "Hello world!\n";
+    printf("Hello world from printf!\n"); 
+    return 0; 
+}
---
+++ hello_world/hello_world.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+#import numpy as np
+#import matplotlib
+#matplotlib.use('Svg')
+#import matplotlib.pyplot as plt
+
+
+
+def main():
+    print('Hello world!')
+    return None
+
+
+
+if __name__ == '__main__':
+    main()
+
